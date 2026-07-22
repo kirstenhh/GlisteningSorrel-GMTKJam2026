035814c04ca157a81f403d11674e7fd38cd95459
yorick <yorick@game-alchemist.ch>
2026-07-22 18:41:40 +0200

    Added Main scene with script

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node
+# THIS IS THE MAIN GAME PROGRAM
+# at least until we decide if we go full OOP or if we do other things
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
