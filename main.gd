d0509fd00549da0713e3d3f936fa2323da828889
Yorick <yorick@game-alchemist.ch>
2026-07-26 17:25:30 +0200

    Small tweaks on Main, can be erased if need be

--- main.gd
+++ main.gd
@@ -22,8 +22,8 @@ func _ready() -> void:
 	$UI/TextPanel.show_message("What the hell was that Flash? Wait where am I?")
 	await $UI/Clock/WhiteLight/Flash.animation_finished
 	$UI/Clock/WhiteLight.visible=false
-	$UI/TextPanel.show_message("(press E to examine, press R to interact or pick up items)")
-	TextManager.push_item_texts("(A (?) bubble indicates that you can examine something, ")
+	$UI/TextPanel.show_message("(press E to examine, press R to interact or pick up items,")
+	TextManager.push_item_texts("a (?) bubble indicates that you can examine something, ")
 	TextManager.push_item_texts("a (!) bubble indicates that you can also interact with or grab it)")
 	TextManager.push_item_texts("Hold on... I kind of recognize this place. It's a laboratory... but what for?")
 	TextManager.push_item_texts("Hey that large computer in the corner is beeping...")
