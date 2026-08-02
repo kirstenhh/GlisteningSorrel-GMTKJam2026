e6d8093edb83eb526d20a77af0ef0b4458d83f95
Yorick <yorick@game-alchemist.ch>
2026-08-02 22:53:24 +0200

    Fixed release

--- main.gd
+++ main.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 		$UI/ExamButton.visible = true
 		$UI/InteractButton.visible = true
 		$UI/VirtualJoystick.visible = true
-	#start_intro()
+	start_intro()
 	pass
 	
 func start_intro():
